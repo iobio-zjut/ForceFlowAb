220f1f6fa627dde797cda796ecbde86267fe8b5b
LZH123LZH123 <18300653018@163.com>
2026-07-15 14:42:28 +0800

    update design and training scripts

--- DesignTest.sh
+++ DesignTest.sh
@@ -4,6 +4,10 @@ do
 done
 
 
+python diffab/tools/relax/run.py --root ./results/codesign_single_H3_0.4  --pipeline pyrosetta
+python  diffab/tools/eval/run.py  --root  ./results/codesign_single_H3_0.4 --pfx rosetta
+
+
 # for i in {0..19}
 # do
 #    python design_testset.py -c ./configs/test/moe/codesign_multicdrs_0.4.yml -b 32 $i
@@ -12,11 +16,4 @@ done
 # python diffab/tools/relax/run.py --root ./results/codesign_multicdrs_0.4  --pipeline pyrosetta
 # python  diffab/tools/eval/run.py  --root  ./results/codesign_multicdrs_0.4 --pfx rosetta
 
-python diffab/tools/relax/run.py --root ./results/codesign_single_H3_0.4  --pipeline pyrosetta
-python  diffab/tools/eval/run.py  --root  ./results/codesign_single_H3_0.4 --pfx rosetta
 
-# for i in {0..19}
-# do
-#    python design_testset.py  -c ./configs/test/moe/codesign_multicdrs_0.4.yml -b 32 $i
-# done
-# python design_testset.py  -c ./configs/test/moe/codesign_multicdrs_0.4.yml -b 32 1
